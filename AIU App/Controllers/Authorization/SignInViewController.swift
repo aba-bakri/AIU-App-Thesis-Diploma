48d45b9263cda0e73230ae25545c31c3c88cf491
aba-bakri <ababakri.ibragimov@gmail.com>
2020-05-06 08:53:13 +0600

    Authorization

--- AIU App/Controllers/Authorization/SignInViewController.swift
+++ AIU App/Controllers/Authorization/SignInViewController.swift
@@ -7,17 +7,62 @@
 //
 
 import UIKit
+import FirebaseAuth
 import Firebase
+
 class SignInViewController: UIViewController {
 
     
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         navigationController?.navigationBar.prefersLargeTitles = true
+        errorLabel.isHidden = true
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        checkIfFieldEmpty()
+    }
+    
+    func login() {
+        Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!) { [weak self] (authResult, error) in
+            guard let strongSelf = self else {return }
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            
+            self?.checkUserInfo()
+        }
+    }
+    
+    func checkUserInfo() {
+        if Auth.auth().currentUser != nil {
+            print(Auth.auth().currentUser?.uid)
+            let storyBoard =  UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyBoard.instantiateViewController(identifier: "mainHome")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+    }
+    
+    func checkIfFieldEmpty() {
+        if emailField.text!.isEmpty == true {
+            errorLabel.text = "Email address can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        if passwordField.text!.isEmpty == true {
+            errorLabel.text = "Password can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        
+        login()
     }
     
 }
--- AIU App/Controllers/Authorization/SignUpViewController.swift
+++ AIU App/Controllers/Authorization/SignUpViewController.swift
@@ -7,13 +7,93 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class SignUpViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var studentIdField: UITextField!
+    @IBOutlet weak var departmentField: UITextField!
+    @IBOutlet weak var phoneField: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         title = "Sign Up"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        errorLabel.isHidden = true
+        
+        
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        
+        checkIfFieldEmpty()
+        signUp()
+    }
+    
+    func signUp() {
+        Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { (authResult, error) in
+            guard let user = authResult?.user, error == nil else { return }
+            
+            let storyBoard =  UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyBoard.instantiateViewController(identifier: "mainHome")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+    }
+    
+    
+    func checkIfFieldEmpty() {
+        errorLabel.isHidden = false
+        
+        if firstNameField.text!.isEmpty {
+            errorLabel.text = "Firstname can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        
+        if lastNameField.text!.isEmpty {
+            errorLabel.text = "Lastname can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        
+        if studentIdField.text!.isEmpty {
+            errorLabel.text = "Student Id can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        
+        if departmentField.text!.isEmpty {
+            errorLabel.text = "Department field can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        
+        if phoneField.text!.isEmpty {
+            errorLabel.text = "Phone field can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        
+        if emailField.text!.isEmpty == true {
+            errorLabel.text = "Email address can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
+        if passwordField.text!.isEmpty == true {
+            errorLabel.text = "Password can't be empty"
+            errorLabel.isHidden = false
+            return
+        }
     }
 }
